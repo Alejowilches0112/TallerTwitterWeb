bb6300a34677a7b56fe2fcddb105022ae8595838
vaorjuela <24valentinaorjuela26@gmail.com>
2020-09-21 16:20:06 -0500

    Finalización Post Controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -55,3 +55,49 @@ async function findAllPost(req, res) {
         });
     }
 }
+
+async function updatePost(req, res) {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Request body is empty!!!!"
+        });
+        return;
+    }
+
+
+    // CREATING THE OBJECT TO PERSIST
+    const updatePostObject = {
+        idUser: req.body.idUser,
+        message: req.body.message,
+        published_date: req.body.published_date
+    }
+
+
+    // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+    dbManager.Post.updatePost(updatePostObject,
+        { where: { idPost: req.params.idPost } })
+        .then(
+            data => {
+                console.log(data);
+                res.send(data);
+            }
+        ).catch(
+            e => {
+                // Print error on console
+                console.log(e);
+                // Send error message as a response 
+                res.status(500).send({
+                    message: "Some error occurred"
+                });
+            }
+        );
+}
+
+
+
+exports.createPost = createPost;
+exports.findAllPost = findAllPost;
+exports.findOnePost = findOnePost;
+exports.deletePostByidPost = deletePostByidPost;
+exports.findPostByUser = findPostByUser;
+exports.updatePost = updatePost;
