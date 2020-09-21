05766cd45efc2296f86899e3e8d566abd0dbe615
vaorjuela <24valentinaorjuela26@gmail.com>
2020-09-21 16:22:00 -0500

    Actualización post route

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var postController = require('../controllers/post.controller');
 
+
+router.get('/', postController.findAllPost);
 /**
- * TASK:
- * CREATE THE ROUTES _________________________________________________________ 
+ * GET Route to find user by id
  */
-  
+router.get('/:idPost', postController.findOnePost);
+/**
+ * POST Route to create user
+ */
+router.post ('/',postController.createPost);
+/**
+ * PUT Route to update an user by id
+ */
+router.put ('/:idPost',postController.updatePost);
+/**
+ * DELETE Route to delete an user by username
+ */
+router.delete ('/:idPost',postController.deletePostByidPost);
+/**
+ * DELETE Route to delete all users
+ */
+router.get ('/:idUser',postController.findPostByUser);
+
+
 
 module.exports = router;
   
